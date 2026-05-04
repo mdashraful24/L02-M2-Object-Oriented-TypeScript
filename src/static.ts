d9e8019ffca5f24fc8a7c76948fd490806d03971
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-05 01:55:56 +0600

    static concept understood

---
+++ src/static.ts
@@ -0,0 +1,20 @@
+// ! Static in typescript
+
+
+class Counter{
+    static count: number =0;
+
+    static increment(){
+        return (Counter.count = Counter.count +1);
+    };
+
+    static decrement(){
+        return (Counter.count = Counter.count -1);
+    };
+};
+
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
